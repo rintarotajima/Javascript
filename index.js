753530a6708d5b07f22f35e3cf08b69e11e52917
rintarotajima <rintarotajima@gmail.com>
2024-02-02 23:44:12 +0900

    Javasciript学習

---
+++ index.js
@@ -0,0 +1,25 @@
+let youtuber = {
+    list: {
+        business: {
+            youtuber: [
+                { name: "しまぶー", age:20, teachProgramming() {} },
+                { name: "あっちゃん", age:37, teachHistory() {} }
+            ],
+            teach() {}
+        },
+        entertainment: {
+            youtuber: [
+                { name: "ヒカキン" },
+                { name: "はじめしゃちょー"}
+            ],
+            makeSmile() {}
+        }
+    },
+    plan() {},
+    uploadVideo() {}
+};
+youtuber.list.business.youtuber[0].teachProgramming();
+
+youtuber.plan();
+
+youtuber.list.entertainment.makeSmile();
